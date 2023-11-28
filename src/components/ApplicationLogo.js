45ccfeab07fa152d70a142fe99ce2490b0f7f5c4
wid100 <helal.womenindigital@gmail.com>
2023-11-28 13:28:17 +0600

    first commit

---
+++ src/components/ApplicationLogo.js
@@ -0,0 +1,3 @@
+const ApplicationLogo = () => <img src="/img/logo.png" alt="" />
+
+export default ApplicationLogo
---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import 'tailwindcss/tailwind.css'
+import '../style/master.css'
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
+
+export default App
