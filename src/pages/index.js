dadeb4e7cc79308f8f3a3708e7d667acb3a55cfa
monojkumar112 <monoj.womenindigital@gmail.com>
2023-12-05 18:07:03 +0600

    home page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
                     </div>
                 </div>
             </header>
-
+            {/* ============= Hero Section ================ */}
             <section>
                 <div className="hero-section">
                     <div className="container">
@@ -62,6 +62,206 @@ export default function Home() {
                                     <button className="hero-btn">
                                         SIGN UP FOR FREE
                                     </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* ======== Input Website ============ */}
+            <section className="qr-input-fleid">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-12">
+                            <div className="qr-input-item-wrapper">
+                                <div className="qr-input-items">
+                                    <div className="qr-input-list-items">
+                                        <div className="qr-input-list-item">
+                                            <span>
+                                                <img
+                                                    src="/img/icon/www.svg"
+                                                    alt=""
+                                                />
+                                            </span>
+                                            <input
+                                                type="text"
+                                                placeholder="Smart URL"
+                                            />
+                                        </div>
+                                        <div className="qr-input-list-item">
+                                            <span>
+                                                <img
+                                                    src="/img/icon/www.svg"
+                                                    alt=""
+                                                />
+                                            </span>
+                                            <input
+                                                type="text"
+                                                placeholder="Smart URL"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="qr-input-list-items">
+                                        <div className="qr-input-list-item">
+                                            <span>
+                                                <img
+                                                    src="/img/icon/www.svg"
+                                                    alt=""
+                                                />
+                                            </span>
+                                            <input
+                                                type="text"
+                                                placeholder="Smart URL"
+                                            />
+                                        </div>
+                                        <div className="qr-input-list-item">
+                                            <span>
+                                                <img
+                                                    src="/img/icon/www.svg"
+                                                    alt=""
+                                                />
+                                            </span>
+                                            <input
+                                                type="text"
+                                                placeholder="Smart URL"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="qr-input-content">
+                                    <h4>Enter your website</h4>
+                                    <p>
+                                        (Your QR Code will be generated
+                                        automatically)
+                                    </p>
+                                    <button className="custom-btn">
+                                        Signup for Free
+                                    </button>
+                                  
+                                </div>
+                                <div className="scan-tracking-item">
+                                        <div className="toggle-button-cover">
+                                            <div
+                                                id="button-3"
+                                                className="button r">
+                                                <input
+                                                    className="checkbox"
+                                                    type="checkbox"
+                                                />
+                                                <div class="knobs"></div>
+                                                <div class="layer"></div>
+                                            </div>
+                                        </div>
+                                        <span>Scan Tracking</span>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-12">
+                            <div className="qr-code-item">
+                                <div className="with-logo">
+                                    <img src="/img/icon/with-logo.png" alt="" />
+                                </div>
+                                <div className="qr-code-img">
+                                    <img src="/img/scan2.png" alt="" />
+                                </div>
+                                <button className="qr-code-download-btn custom-btn">
+                                DOWNLOAD
+    
+                                </button>
+                            </div>
+                         
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* ============= vCard Plus Roadmap =========== */}
+            <section className="vCard-roadmap">
+                <div className="container">
+                    <div className="vCard-roadmap-wrapper">
+                        <div className="vcard-roadmap-title">
+                            <h4>How do I create a free vCard Plus?</h4>
+                        </div>
+                        <div className="vcard-roadmap-content">
+                            <div className="vcard-roadmap-content-items">
+                                <div className="vcard-roadmap-content-item">
+                                    <div className="roadmap-img">
+                                        <img
+                                            src="/img/home/roadmap-1.png"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                                <div className="vcard-roadmap-content-item">
+                                    <h3>Create a free account</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit
+                                        in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint
+                                        occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim
+                                        id est laborum.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="vcard-roadmap-content-items vcard-roadmap-content-items-2">
+                                <div className="vcard-roadmap-content-item">
+                                    <h3>Fill in the details</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit
+                                        in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint
+                                        occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim
+                                        id est laborum.
+                                    </p>
+                                </div>
+                                <div className="vcard-roadmap-content-item vcard-roadmap-content-item2">
+                                    <div className="roadmap-img">
+                                        <img
+                                            src="/img/home/roadmap-2.png"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="vcard-roadmap-content-items">
+                                <div className="vcard-roadmap-content-item ">
+                                    <div className="roadmap-img">
+                                        <img
+                                            src="/img/home/roadmap-3.png"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                                <div className="vcard-roadmap-content-item vcard-roadmap-content-item3">
+                                    <h3>Download the QR Code</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit
+                                        in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint
+                                        occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim
+                                        id est laborum.
+                                    </p>
                                 </div>
                             </div>
                         </div>
